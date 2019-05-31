edf037c2603751a86d9b34819fb1ccd0b97b64d4
ishiyama Sayuri <50101823+ishiyamasayuri@users.noreply.github.com>
2019-05-31 16:59:34 +0800

    Revert "Revert "ASMC V0.32Pre "事件" 更新""
    This reverts commit 135f8a2d325c11a5470a0928540ab236358dc64b.

---
+++ src/com/ksptooi/ASMC/eventManager/EventHandler.java
@@ -0,0 +1,18 @@
+package com.ksptooi.ASMC.eventManager;
+
+import com.ksptooi.ASMC.event.CommandEvent;
+
+public class EventHandler{
+
+	
+	public CommandEvent event_onCommand(CommandEvent ce) {
+		
+		
+		
+		return ce;
+		
+	}
+
+	
+	
+}
